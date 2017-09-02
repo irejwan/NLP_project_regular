55321db7a4d49593b1228fa72437d4d75397fb01
Mor Cohen <morco@Mors-MacBook-Air.local>
2017-09-02 16:42:50 +0300

    added sample & concat data

--- data_utils.py
+++ data_utils.py
@@ -50,37 +50,14 @@ def generate_sentences(DATA_AMOUNT, alphabet_map):
     return X_train, y_train, X_test, y_test
 
 
-def generate_random_strings(max_length, num_of_sents, alphabet):
-    random_lengths = np.random.randint(low=1, high=max_length, size=num_of_sents)
-    return [rstr.rstr(alphabet, length) for length in random_lengths]
-
-
-def filter_out_by_allowed_pos(sent, alphabet_map):
-    allowed_pos = [alphabet_map[pos] for pos in ['N', 'V', 'J', 'A', 'D', 'S', 'I', 'T']]
-    for pos in sent:
-        if pos not in allowed_pos:
-            return False
-    return True
-
-
 def get_penn_pos_data(total_num_of_sents, alphabet_map):
-    grammatical_sents = read_conll_pos_file("Penn_Treebank/train.gold.conll")
-    grammaticals = []
-    left = total_num_of_sents // 2
-    curr_idx = 0
-    while left > 0 and curr_idx < len(grammatical_sents):
-        curr_grammaticals = list(grammatical_sents)[curr_idx:curr_idx + left]
-        # grammaticals += list(filter(lambda x: filter_out_by_allowed_pos(x, alphabet_map), curr_grammaticals))
-        grammaticals += curr_grammaticals
-        curr_idx += left
-        left = (total_num_of_sents // 2) - len(grammaticals)
+    grammatical_sents = read_conll_pos_file("../Penn_Treebank/train.gold.conll")
+    grammaticals = sample_concat_sentences(grammatical_sents, total_num_of_sents // 2)
 
     ungrammaticals = []
     left = total_num_of_sents // 2
-    # alphabet = list(set(alphabet_map.keys()).intersection({'N', 'V', 'J', 'A', 'D', 'S', 'I', 'T'}))
-    alphabet = list(set(alphabet_map.keys()))
     while left > 0:
-        curr_ungrammaticals = generate_random_strings(config.Data.max_len.int, left, alphabet)
+        curr_ungrammaticals = generate_random_strings(config.Data.max_len.int, left, list(set(alphabet_map.keys())))
         curr_ungrammaticals = list(map(lambda sent: [alphabet_map[s] for s in list(sent)], curr_ungrammaticals))
         ungrammaticals += list(filter(lambda sent: filter_out_grammatical_sentences(sent, grammatical_sents),
                                       curr_ungrammaticals))
@@ -89,23 +66,6 @@ def get_penn_pos_data(total_num_of_sents, alphabet_map):
     data = grammaticals + ungrammaticals
     labels = np.array([1] * len(grammaticals) + [0] * len(ungrammaticals))
     return data, labels
-
-
-def generate_simple_english_grammar(num_of_sents, max_seq_len):
-    pcfg = PCFG.from_file('grammar.txt')
-    output = set()
-    while len(output) < num_of_sents:
-        sent = pcfg.random_sent()
-        if len(sent.split()) < max_seq_len:
-            output.add(sent)
-    return output
-
-
-def filter_by_regex(sent, regex):
-    pattern = re.compile(regex)
-    if pattern.match(sent):
-        return False
-    return True
 
 
 def get_regex_sentences(num_sents, alphabet_map):
@@ -141,6 +101,53 @@ def get_regex_sentences(num_sents, alphabet_map):
     return data, labels
 
 
+def generate_random_strings(max_length, num_of_sents, alphabet):
+    random_lengths = np.random.randint(low=1, high=max_length, size=num_of_sents)
+    return [rstr.rstr(alphabet, length) for length in random_lengths]
+
+
+def filter_by_pos(sent, alphabet_map):
+    allowed_pos = [alphabet_map[pos] for pos in ['N', 'V', 'J', 'A', 'D', 'S', 'I', 'T']]
+    for pos in sent:
+        if pos not in allowed_pos:
+            return False
+    return True
+
+
+def sample_concat_sentences(sents, num_of_sents):
+    def sample(sentences):
+        rand_idx = np.random.randint(0, len(sentences), 1)
+        return sentences[rand_idx]
+
+    def concat(sentences):
+        rand_idx1, rand_idx2 = np.random.randint(0, len(sentences), 2)
+        conj = pos_category_to_num[pos_category_map['CC']]
+        return sentences[rand_idx1] + [conj] + sentences[rand_idx2]
+
+    output = []
+    for i in range(num_of_sents):
+        action = np.random.choice([sample, concat])
+        output.append(action(sents))
+    return output
+
+
+def generate_simple_english_grammar(num_of_sents, max_seq_len):
+    pcfg = PCFG.from_file('grammar.txt')
+    output = set()
+    while len(output) < num_of_sents:
+        sent = pcfg.random_sent()
+        if len(sent.split()) < max_seq_len:
+            output.add(sent)
+    return output
+
+
+def filter_by_regex(sent, regex):
+    pattern = re.compile(regex)
+    if pattern.match(sent):
+        return False
+    return True
+
+
 def random_trans(sentence, alphabet):
     result = copy(sentence)
     num_trans = np.random.randint(1, min(2, len(sentence)))
@@ -172,13 +179,13 @@ def read_conll_pos_file(path):
     """
     Takes a path to a file and returns a list of tags
     """
-    sents = set()
+    sents = []
     with open(path, "r") as f:
         curr = []
         for line in f:
             line = line.strip()
             if line == "":
-                sents.add(tuple(curr))
+                sents.append(curr)
                 curr = []
             else:
                 tokens = line.strip().split("\t")
